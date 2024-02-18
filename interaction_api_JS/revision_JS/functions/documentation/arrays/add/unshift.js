07f02b7babfb63fa24395aa82dcab01d7a29daf7
Daniel <danielfelipe2905@gmail.com>
2024-02-18 17:28:18 -0300

    initial skeleton added

--- interaction_api_JS/revision_JS/functions/documentation/arrays/add/unshift.js
+++
@@ -1,6 +0,0 @@
-// Permite adicionar informações no começo de um array
-
-array = [1,2,3,4,5]
-
-array.unshift(0)
-console.log(array)
--- interaction_api_JS/revision_JS/functions/documentation/arrays/remove/pop.js
+++
@@ -1,9 +0,0 @@
-// Remove o último dado
-
-array = [1,2,3,4,5]
-
-for (let i = 0; i < array.length - 2; i++) {
-    array.pop()   
-}
-
-console.log(array)
